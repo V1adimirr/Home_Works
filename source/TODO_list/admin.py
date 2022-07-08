8dfdbcc70f39acf39702feccf6123763b22f0e35
Vladimir <vladimirpedenko936@gmail.com>
2022-07-09 00:02:32 +0600

    "создал модели, зарегистрировал в админке"

--- source/TODO_list/admin.py
+++ source/TODO_list/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from TODO_list.models import TaskModel, Type, Status
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['short_de', 'description', 'status', 'type']
+    list_filter = ['status', 'type']
+    search_fields = ['type']
+    fields = ['short_de', 'description', 'status', 'type', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(TaskModel, TaskAdmin)
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['type']
+    fields = ['type']
+
+
+admin.site.register(Type, TypeAdmin)
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['status']
+    fields = ['status']
+
+
+admin.site.register(Status, StatusAdmin)
--- source/TODO_list/models.py
+++ source/TODO_list/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    class Meta:
+        abstract = True
+
+
+class TaskModel(BaseModel):
+    short_de = models.CharField(max_length=100, verbose_name='Краткое описание')
+    description = models.TextField(max_length=2000, null=False, blank=False, verbose_name='Описание')
+    status = models.ForeignKey("TODO_list.Status", related_name='Status', on_delete=models.PROTECT,
+                               verbose_name='Статус')
+    type = models.ForeignKey("TODO_list.Type", related_name='Type', on_delete=models.PROTECT,
+                             verbose_name='Тип')
+
+    def __str__(self):
+        return f"{self.id}. {self.short_de} : {self.status}"
+
+    class Meta:
+        db_table = 'Tasks'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+
+class Type(BaseModel):
+    type = models.CharField(max_length=30, verbose_name='Тип')
+
+    def __str__(self):
+        return f"{self.id}, {self.type}"
+
+    class Meta:
+        db_table = 'Type'
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
+
+
+class Status(BaseModel):
+    status = models.CharField(max_length=30, verbose_name='Статус')
+
+    def __str__(self):
+        return f"{self.id}, {self.status}"
+
+    class Meta:
+        db_table = 'Status'
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
