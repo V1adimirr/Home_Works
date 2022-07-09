759c26941a0ca04544484f7b8ac727b3675fe8a8
Vladimir <vladimirpedenko936@gmail.com>
2022-07-09 16:56:31 +0600

    "добавил просмотр одной задачи"

--- source/TODO_list/urls.py
+++ source/TODO_list/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from TODO_list.views import IndexView
+from TODO_list.views import IndexView, TaskView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('task/<int:pk>/view/', TaskView.as_view(), name='view')
 ]
--- source/TODO_list/views.py
+++ source/TODO_list/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.views import View
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.base import View, TemplateView
 
 from TODO_list.models import TaskModel
+
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
@@ -10,6 +11,11 @@ class IndexView(View):
         return render(request, "index.html", context)
 
 
+class TaskView(TemplateView):
+    template_name = "task_view.html"
 
-
-
+    def get_context_data(self, **kwargs):
+        pk = kwargs.get("pk")
+        task = get_object_or_404(TaskModel, pk=pk)
+        kwargs["task"] = task
+        return super().get_context_data(**kwargs)
